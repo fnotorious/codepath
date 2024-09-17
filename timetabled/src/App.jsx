de5bee8a0dc9975fecebef3f8ef715524f581cbe
Firas Natour <firasnatour@Firass-MacBook-Air.local>
2024-09-16 21:34:59 -0400

    (PREWORK 1): Timetabled complete

---
+++ timetabled/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Calendar from './components/Calendar'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <h2>This is a secondary header</h2>      
+      <Calendar />
+    </div>
+  )
+}
+
+export default App
