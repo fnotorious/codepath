f99f29159b39ce452eda3075a02ae237579b1ffa
Firas Natour <firasnatour@Firass-MacBook-Air.local>
2024-09-30 23:07:18 -0400

    (PREWORK 3): Creating project folder

---
+++ barista-app/src/components/BaristaForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const BaristaForm = () => {
+  return (
+    <div>
+        <form>
+
+        </form>
+        <button className="button submit">
+            
+        </button>
+    </div>
+  )
+}
+
+export default BaristaForm
