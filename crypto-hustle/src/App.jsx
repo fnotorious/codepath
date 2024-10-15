48b641563bad18f199e001e0bec468c698551f60
Firas Natour <firasnatour@Firass-MacBook-Air.local>
2024-10-15 18:49:18 -0400

    (PREWORK 5): Complete

--- crypto-hustle/src/App.jsx
+++ crypto-hustle/src/App.jsx
@@ -1,28 +1,72 @@
 import { useState, useEffect } from 'react'
-import viteLogo from '/vite.svg'
+import CoinInfo from './components/CoinInfo';
 import './App.css'
 
 function App() {
-  const [list, setList] = useState(null)
+  const [list, setList] = useState(null);
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
   const API_KEY = import.meta.env.VITE_APP_API_KEY;
-  const url = `https://min-api.cryptocompare.com/data/all/coinlist?&api_key=${API_KEY}`;
+  const url = `https://min-api.cryptocompare.com/data/all/coinlist?api_key=${API_KEY}`;
 
   useEffect(() => {
-    const fetchAllCoinData = async () => {
-      const response = fetch(url);
-      const data = await response.json;
+    const fetchAllCoinData = async() => {
+      const response = await fetch(url);
+      const data = await response.json();
+      
       setList(data);
     }
 
     fetchAllCoinData().catch(console.error);
   }, []);
 
+  const searchItems = searchValue => {
+    setSearchInput(searchValue);
+    if (searchValue !== "") {
+      const filteredData = Object.keys(list.Data).filter((item) => 
+        Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchValue.toLowerCase())
+      )
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(Object.keys(list.Data));
+    }
+  };
+
   return (
     <div className="whole-page">
       <h1>My Crypto List</h1>
-        <ul>
-            
-        </ul>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(inputString) => searchItems(inputString.target.value)}
+      />
+      <ul>
+      {searchInput.length > 0
+        ? filteredResults.map((coin, index) => 
+            list.Data[coin].PlatformType === "blockchain" ? 
+            <CoinInfo
+              image={list.Data[coin].ImageUrl}
+              name={list.Data[coin].FullName}
+              symbol={list.Data[coin].Symbol}
+              key={index}
+            />
+            : null
+          )
+        : list && Object.entries(list.Data).map(([coin], index) => 
+            list.Data[coin].PlatformType === "blockchain" ? 
+            <CoinInfo
+              image={list.Data[coin].ImageUrl}
+              name={list.Data[coin].FullName}
+              symbol={list.Data[coin].Symbol}
+              key={index}
+            />
+        : null
+      )}
+      </ul>
     </div>
   )
 }
---
+++ crypto-hustle/src/components/CoinInfo.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
+
+const CoinInfo = ({image, name, symbol}) => {
+    const [price, setPrice] = useState(null);
+    
+    const API_KEY = import.meta.env.VITE_APP_API_KEY;
+    const url = `https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=USD&api_key=${API_KEY}`;
+
+    useEffect(() => {
+        const getCoinPrice = async() => {
+            const response = await fetch(url);
+            const data = await response.json();
+
+            setPrice(data);
+        }
+
+        getCoinPrice().catch(console.error);
+    }, [symbol]);
+
+    return (
+        <div>
+            {price ? (
+                <li className="main-list" key={symbol}>
+                    <img
+                        className="icons"
+                        src={`https://www.cryptocompare.com${image}`}
+                        alt={`Small icon for ${name} crypto coin`}
+                    />
+                    {name} 
+                    <span className="tab"></span> ${price.USD} USD
+                </li>
+                ) : null
+            }
+        </div>
+    )
+}
+
+export default CoinInfo
